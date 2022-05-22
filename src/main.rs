5697d883246a5f96047c6c1f2ffab1ecbf8f756b
kodylow <kodylow7@gmail.com>
2022-05-21 17:29:28 -0700

    cmd line args

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use std::env;
 
 fn main() {
     // iterators produce a series of values, collect turns the vals into a collection
+    //2 arguments, query and filename. index 0 is this program's name
+
+    //example: cargo run test sample.txt
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
