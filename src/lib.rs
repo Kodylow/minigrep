5369dc1b1f99f36bba0aa929473796dc758110c5
kodylow <kodylow7@gmail.com>
2022-05-22 10:58:41 -0700

    finished minigrep

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,56 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 // runs execution logic of program
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // read file contents into a single string woah that's cool
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
 
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
 }
 
+// search function of grep, finds query within contents
+// lifetime required because data referenced BY a slice needs to be valid
+// compiler would assume we're making string slices of query vs contents otherwise
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
+}
+
+// search function of grep, case_insensitive if env variable triggered
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line)
+        }
+    }
+
+    results
+}
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 // extracts and returns a Config struct from 2 cmd linearguments,
@@ -29,6 +66,41 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust: ",], search_case_insensitive(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,14 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(|err| {
         // unwrap_or_else lets you handle error message
         // instead of throwing panic on bad unwraps
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         // A nonzero exit status is a convention to signal to the
         // that the program exited with an error state.
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
